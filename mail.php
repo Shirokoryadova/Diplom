8f2bd592271a172517d29af4d08417d80e0b5194
ShirokoryadovaK <shirokoryadovak@gmail.com>
2020-10-03 13:37:18 +0300

    favicon added

--- mail.php
+++ mail.php
@@ -6,9 +6,9 @@ $method = $_SERVER['REQUEST_METHOD'];
 $c = true;
 if ( $method === 'POST' ) {
 
-	$project_name = trim($_POST["project_name"]);
-	$admin_email  = trim($_POST["admin_email"]);
-	$form_subject = trim($_POST["form_subject"]);
+	$project_name = "Dmitry Alekseev";
+	$admin_email  = "shirokoryadovak@gmail.com";
+	$form_subject = "Request";
 
 	foreach ( $_POST as $key => $value ) {
 		if ( $value != "" && $key != "project_name" && $key != "admin_email" && $key != "form_subject" ) {
@@ -22,9 +22,9 @@ if ( $method === 'POST' ) {
 	}
 } else if ( $method === 'GET' ) {
 
-	$project_name = trim($_GET["project_name"]);
-	$admin_email  = trim($_GET["admin_email"]);
-	$form_subject = trim($_GET["form_subject"]);
+	$project_name = "Dmitry Alekseev";
+	$admin_email  = "shirokoryadovak@gmail.com";
+	$form_subject = "Request";
 
 	foreach ( $_GET as $key => $value ) {
 		if ( $value != "" && $key != "project_name" && $key != "admin_email" && $key != "form_subject" ) {
